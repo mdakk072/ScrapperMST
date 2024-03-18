bdde9a1929f13efd3e11b85b382b3ae1bacadc08
mdakk072 <mounir.dakkak@gmail.com>
2024-03-18 15:55:12 -0400

    Refactor and Enhance Project Structure
    - Refactored CSS for improved clarity and modularity.
    - Extracted Flash and Task functionality into separate components for better code organization.
    - Changed application port to 8000 for consistency and compatibility.
    - Added new background images to enhance the UI.

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from flask_todolist.services import create_database,add_mock_tasks_for_each_user
 if __name__ == '__main__':
     create_database(app)
     add_mock_tasks_for_each_user(app,40)
-    app.run(debug=False, port=80, host='0.0.0.0')  # Run the app
+    app.run(debug=True, port=8000, host='0.0.0.0')  # Run the app
